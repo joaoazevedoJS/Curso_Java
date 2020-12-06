a1083a329f7c6d690000dd5e258309c2f45c15eb
Joao Azevedo <devjoaoazevedo@gmail.com>
2020-12-06 12:23:16 -0300

    Add 'Fundamentos' and 'Praticas'

---
+++ pratica/src/fundamentos/Pratica01.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Pratica01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+		
+		System.out.println("Digite uma mensagem: ");
+		
+		String i = sc.nextLine();
+		
+		System.out.println(i);
+	}	
+}
