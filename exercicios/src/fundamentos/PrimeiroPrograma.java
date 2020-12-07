51145962e1a719fa440b0e640bd20ea3faea0a66
Joao Azevedo <devjoaoazevedo@gmail.com>
2020-12-07 06:08:42 -0300

    Add news projects closed

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,7 +1,27 @@
 package fundamentos;
 
+/**
+ * Essa classe representa...
+ * 
+ * Isso é um javadoc
+ * 
+ * @author joaoazevedojs <@email.com>
+ * @since JDK1.0
+ */
 public class PrimeiroPrograma {
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		// Uma sentença de código termina com ;
 		System.out.println("Hello World!!!");
+		
+		/*
+		 * Linha 1
+		 * Linha 2
+		 */
+		
+		// System.out.println("Hello World!!!");
 	}
 }
