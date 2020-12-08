1be6e8a8c91b56e2c781411737ad81b588911c80
Joao Azevedo <devjoaoazevedo@gmail.com>
2020-12-08 06:39:56 -0300

    Add new classes in 'fundamentos'

---
+++ exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,29 @@
+package fundamentos;
+
+public class Inferencia {
+	public static void main(String[] args) {
+		double a = 4.5; // declaração e inicialização
+		System.out.println(a);
+		
+		a = 12;
+		System.out.println(a);
+		
+		// a = "..." // Error
+		
+		// A inferência pega o tipo da variável a partir do valor
+		var b = 7.7;
+		
+		// c é do tipo String
+		var c = "Texto";
+		System.out.println(c);
+
+		c = "Outro Texto";
+		System.out.println(c);
+		
+		// c = 4.5 // Error
+		
+		double d; // variável foi declarada
+		d = 123.65; // variável foi inserida
+		System.out.println(d); // variável foi usada
+	}
+}
---
+++ exercicios/src/fundamentos/Temperatura.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Temperatura {
+	public static void main(String[] args) {
+		// Formula = (ºF - 32) * 5/9 = ºC
+		Scanner scan = new Scanner(System.in); 
+				
+		final int AJUSTE = 32;
+		final double FATOR = 5.0 / 9.0;
+		
+		System.out.print("Digite uma temperatura em fahrenheit: ");
+		
+		final String INPUT = scan.nextLine();
+		
+		final double FAHRENHEIT = Double.parseDouble(INPUT);
+	
+		final double CELSIUS = (FAHRENHEIT - AJUSTE) * FATOR;
+		
+		System.out.println("Fahrenheit de " + FAHRENHEIT + " em celsius é " + CELSIUS);
+	}
+}
